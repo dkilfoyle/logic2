261e07f6c337b74e6bf491abc2cc3f6bd2e13c58
deank <dean1222@xtra.co.nz>
2021-02-19 00:17:16 +1300

    mips datapath

--- src/files/mips.v
+++ src/files/mips.v
@@ -164,6 +164,51 @@ module Aludec (
     endcase
 endmodule
 
+module Datapath (
+  input clk, reset,
+  input memtoreg, pcsrc,
+  input alusrc, regdst,
+  input regwrite, jump,
+  input [2:0] alucontrol,
+  output zero,
+  output [31:0] pc,
+  input [31:0] instr,
+  output [31:0] aluout, writedata,
+  input [31:0] readdata
+);
+  wire [4:0] writereg;
+  wire [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
+  wire [31:0] signimm, signimmsh;
+  wire [31:0] srca, srcb;
+  wire [31:0] result;
+
+  // next PC logic
+  flopr #(32) pcreg(clk, reset, pcnext, pc);
+  adder pcadd1 (pc, 32'b100, pcplus4); // pcplus4 = pc + 4
+  sl2 immsh(signimm, signimmsh);
+  adder pcadd2(pcplus4, signimmsh, pcbranch); // pcbranch = pcplus4 + (offset * 4)
+  mux2 #(32) pcbrmux(pcplus4, pcbranch, pcsrc,  pcnextbr); // pcnextbr = pcsrc ? pcbranch : pcplus4
+  mux2 #(32) pcmux(pcnextbr, {pcplus4[31:28],  instr[25:0], 2'b00}, jump, pcnext);
+
+  // register file logic
+  regfile rf(clk, regwrite, 
+    .A1(instr[25:21]), // rs
+    .A2(instr[20:16]), // rt
+    .A3(writereg),
+    .WD3(result),
+    // outputs
+    .RD1(srca),
+    .RD2(writedata));
+  mux2 #(5) wrmux(instr[20:16], instr[15:11],  regdst, writereg);
+  mux2 #(32) resmux(aluout, readdata,  memtoreg, result);
+  signext se(instr[15:0], signimm);
+
+// ALU logic
+mux2 #(32) srcbmux(writedata, signimm, alusrc,
+srcb);
+alu alu(srca, srcb, alucontrol,
+aluout, zero);
+endmodule
 
 module Main (
   input [2:0] F,
